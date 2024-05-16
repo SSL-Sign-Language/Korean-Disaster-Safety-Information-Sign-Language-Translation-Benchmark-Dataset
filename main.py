4247f89417bff489597b3e2b2afc7ca85979c0a2
ssl_sign <ssl.sign.language@gmail.com>
2024-05-16 21:10:27 +0900

    Modify logging

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import logging
 from src.sign_processor import SignProcessor
 
 def main():
+    logging.basicConfig(
+    format = '%(asctime)s:%(levelname)s:%(message)s',
+    datefmt = '%m/%d/%Y %I:%M:%S %p',
+    level = logging.INFO)
+    
     sign_processor = SignProcessor()
     sign_processor.start()
 
